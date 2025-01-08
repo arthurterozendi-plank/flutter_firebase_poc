2aa626f6b21040b799f3c916682f50430df77e34
arthurterozendi-plank <arthur.terozendi@joinplank.com>
2025-01-08 10:50:27 -0300

    chat cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,48 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import {onRequest} from "firebase-functions/v2/https";
+import * as functions from 'firebase-functions';
+import * as logger from "firebase-functions/logger";
+import OpenAI from "openai";
 
-// import {onRequest} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
+const openai = new OpenAI({
+  apiKey: functions.config().openai.apikey,
+});
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const healthCheck = onRequest({
+  region: "us-central1",
+  cors: true,
+  maxInstances: 10
+}, (_request, response) => {
+  logger.info("Health check");
+  response.send("OK");
+});
+
+export const chatWithOpenAI = onRequest({
+  region: "us-central1",
+  cors: true,
+  maxInstances: 10
+}, async (request, response) => {
+  logger.info("Chat with OpenAI");
+
+  const prompt = request.body.prompt;
+
+  if (!prompt) {
+    response.status(400).send("Prompt is required");
+    return;
+  }
+
+  try {
+    const chatCompletion = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: prompt}],
+    });
+
+    logger.info(chatCompletion.choices[0].message?.content);
+    response.send(chatCompletion.choices[0].message?.content);
+  } catch (error) {
+    logger.error(error);
+    response.status(500).send("Error calling OpenAI");
+  }
+});
